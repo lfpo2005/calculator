c00cec26547e61aea49fb10565bc2419c1a949f2
Fernando Oliveira <47955596+lfpo2005@users.noreply.github.com>
2021-11-28 22:27:19 -0300

    percent button logic

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -23,6 +23,8 @@ class CalcController {
 
     }, 1000);
 
+    this.setLastNumberToDisplay();
+
   }
 
   addEventListenerAll(element, events, fn) {
@@ -39,11 +41,15 @@ class CalcController {
 
     this._operation = [];
 
+    this.setLastNumberToDisplay();
+
   }
 
   clearEntry() {
 
     this._operation.pop();
+
+    this.setLastNumberToDisplay();
 
   }
 
@@ -79,12 +85,33 @@ class CalcController {
     
     calc(){
 
-      let last = this._operation.pop();
-      
+      let last = '';
+
+      if (this._operation.length > 3){
+
+        last = this._operation.pop();
+
+      }
+
+
       let result = eval(this._operation.join(""));
 
-      this._operation = [result, last];
+      if (last == "%"){
 
+        result /= 100;
+
+        this._operation = [result];
+
+      }else{
+
+       
+        this._operation = [result];
+
+        if (last) this._operation.push(last);
+  
+      }
+      
+     
       this.setLastNumberToDisplay();
 
   }
@@ -100,6 +127,8 @@ class CalcController {
               break;
           }
         }
+
+        if (!lastNumber) lastNumber = 0;
 
         this.displayCalc = lastNumber;
 
@@ -175,7 +204,7 @@ class CalcController {
         this.addOperation("%");
         break;
       case "igual":
-        
+        this.calc();
         break;
       case "ponto":
         this.addOperation(".");
